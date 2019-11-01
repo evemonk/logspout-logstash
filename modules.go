004bde7615d26bae883d8a76335f612c07669134
Igor Zubkov <igor.zubkov@ag-force.com>
2019-11-02 01:31:10 +0200

    First version

---
+++ modules.go
@@ -0,0 +1,11 @@
+// https://github.com/gliderlabs/logspout/blob/master/custom/modules.go
+
+package main
+
+import (
+	_ "github.com/gliderlabs/logspout/adapters/syslog"
+	_ "github.com/gliderlabs/logspout/transports/tcp"
+	_ "github.com/gliderlabs/logspout/transports/tls"
+	_ "github.com/gliderlabs/logspout/transports/udp"
+	_ "github.com/looplab/logspout-logstash"
+)
